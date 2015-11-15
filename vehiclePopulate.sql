28ce941cf42b433f6e120bacca3b32d255f33047
gyoque <g.yoque@gmail.com>
2015-11-15 15:36:29 -0800

    Added vehiclePopulate.sql

---
+++ vehiclePopulate.sql
@@ -0,0 +1,11 @@
+insert into Vehicle values('2T1BU4EE9AC426933', 10.0, 'Honda', 'Civic', 2014, 'Automatic', 5);
+insert into Vehicle values('4V5J32HEXYN877732', 15.0, 'Kia', 'Rio', 2013, 'Automatic', 4);
+insert into Vehicle values('1GDG5V1246F476458', 12.0, 'Nissan', 'Versa', 2015, 'Automatic', 4);
+insert into Vehicle values('2NKMHZ7X77M937098', 11.0, 'Toyota', 'Corolla', 2010, 'Automatic', 5);
+insert into Vehicle values('1FMNU41L82EC63056', 20.0, 'Volkswagen', 'Jetta', 2014, 'Manual', 5);
+insert into Vehicle values('1XKAD09X39J266553', 13.0, 'Ford', 'Fusion', 2012, 'Automatic', 5);
+insert into Vehicle values('1NKWXBTX92J809230', 13.50, 'Chrysler', '300', 2014, 'Automatic', 5);
+insert into Vehicle values('1FTRF18W6YNB53307', 20.0, 'Cadillac', 'ATS', 2015, 'Automatic', 5);
+insert into Vehicle values('3WKDDU9XX4F051947', 18.0, 'Ford', 'Mustang', 2013, 'Automatic', 4);
+insert into Vehicle values('1GDHK34JXVF063012', 17.50, 'Chevy', 'Volt', 2015, 'Automatic', 4);
+insert into Vehicle values('1B4GP45L8YB627191', 19.0, 'BMW', '5 Series', 2015, 'Automatic', 5);
