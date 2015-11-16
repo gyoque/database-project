c9235a2db06d80f1ebb1464d46ebf749c29cf2e4
gyoque <g.yoque@gmail.com>
2015-11-15 16:16:43 -0800

    added reservation

--- vehiclePopulate.sql
+++ vehiclePopulate.sql
@@ -9,3 +9,11 @@ insert into Vehicle values('1FTRF18W6YNB53307', 20.0, 'Cadillac', 'ATS', 2015, '
 insert into Vehicle values('3WKDDU9XX4F051947', 18.0, 'Ford', 'Mustang', 2013, 'Automatic', 4);
 insert into Vehicle values('1GDHK34JXVF063012', 17.50, 'Chevy', 'Volt', 2015, 'Automatic', 4);
 insert into Vehicle values('1B4GP45L8YB627191', 19.0, 'BMW', '5 Series', 2015, 'Automatic', 5);
+
+
+
+insert into Reservation values(12345678,'69KED76646','07/30/2015 11:00','08/05/2015 12:00','Tacoma,WA', 420.0,'2T1BU4EE9AC426933');
+insert into Reservation values(23476546,'SPELLT959ZE','08/12/2015 12:00', '08/20/2015 15:00', 'Los Angeles,CA', 367.0,'3WKDDU9XX4F051947');
+insert into Reservation values(23487987,'575531227','08/13/2015 13:00','08/14/2015 13:00','Salem,OR',115.0,'1B4GP45L8YB627191');
+insert into Reservation values(36849905,'SPELLT959ZE','09/09/2015 08:00', '09/09/2015 15:00', 'Seattle,WA',120.0,'1FTRF18W6YNB53307');
+insert into Reservation values(98903456,'838595338','09/09/2015 15:00', '09/21/2015 10:00', 'Boston,MA',500.0,'4V5J32HEXYN877732');
